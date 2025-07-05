525cb603c3aa07fe36063eb2b68c3dc3f6737413
mikbal54 <mikbal@sabanciuniv.edu>
2025-07-05 11:12:35 +0300

    git

---
+++ Content/index.js
@@ -0,0 +1,123 @@
+﻿var servesAddress = "http://localhost:9000"
+var refreshTime = 3000;
+var shortUpdateTime = 60;
+var activeMinerSelectionSeconds = 9
+
+angular.module('minerMonitorApp', [])
+  .controller('collapsibleController', function ($scope) {
+
+      var minerMonitor = this;
+
+      minerMonitor.shortUpdateData = []
+
+      minerMonitor.activeMiners = [
+          //{
+          //    index: 0,
+          //    name: "developer",
+          //    algo: "ethash",
+          //    hashrate: 142.5,
+          //},
+          // {
+          //     index: 1,
+          //     name: "tester",
+          //     algo: "equihash",
+          //     hashrate: 155
+          // }
+      ]
+
+      minerMonitor.passiveMiners = [
+
+      ]
+
+      minerMonitor.filterActiveMiners = function () {
+
+          
+          var seconds = Math.floor((new Date).getTime()/1000);
+          var inlastseconds = _.filter(minerMonitor.shortUpdateData, function (item) {
+              if (seconds - item.time > activeMinerSelectionSeconds)
+                  return true
+          });
+
+          minerMonitor.activeMiners = _.uniq(inlastseconds, false, function (item) {
+              return item.name;
+          });
+
+          var b = 2
+      }
+
+
+      minerMonitor.updateStats = function () {
+
+          minerMonitor.getMinerStats(shortUpdateTime, function (result) {
+              $scope.$apply();
+          });
+
+          
+      }
+
+      minerMonitor.getMinerStats = function (seconds, extraCall) {
+
+          $.ajax({
+              url: servesAddress +"/minerStats/"+seconds ,
+              success: function (result) {
+                  minerMonitor.shortUpdateData = result
+                  activeMiners = minerMonitor.filterActiveMiners()
+                  extraCall(result)
+              },
+              dataType: "json"
+          });
+
+      }
+
+      minerMonitor.totalHashrates = {}
+
+      minerMonitor.totalHashrateString = function () {
+          minerMonitor.calculateTotalHashrate()
+          var str = "";
+          for (var a in minerMonitor.totalHashrates) {
+              str += a;
+              str += "= ";
+              str += minerMonitor.totalHashrates[a];
+              str += ", ";
+          }
+          str = str.substring(0, str.length - 2);
+          return str
+      }
+
+      minerMonitor.totalActiveMiners = function () {
+
+          var seconds = Math.floor((new Date).getTime()/1000);
+          // actives in the last 30 seconds
+          var activeMiners = _.pluck(_(minerMonitor.shortUpdateData).filter(function (val) {
+              if (seconds - val.time < activeMinerSelectionSeconds)
+                  return val;
+          }));
+          activeMiners = _.uniq(activeMiners)
+
+          
+          return activeMiners.length; //TODO: calculate active miners
+      }
+
+      minerMonitor.totalMiners = function () {
+          var uniqueMiners = _.pluck(minerMonitor.shortUpdateData, "name");
+          uniqueMiners = _.uniq(uniqueMiners)
+          return uniqueMiners.length
+      }
+
+      minerMonitor.calculateTotalHashrate = function () {
+
+          minerMonitor.totalHashrates = {}
+          for (var a in minerMonitor.miners) {
+
+              if( minerMonitor.miners[a].algo in minerMonitor.totalHashrates )
+                  minerMonitor.totalHashrates[minerMonitor.miners[a].algo] += minerMonitor.miners[a].hashrate
+              else
+                  minerMonitor.totalHashrates[minerMonitor.miners[a].algo] = minerMonitor.miners[a].hashrate
+
+          }
+
+      }
+
+
+  });
+
---
+++ Program.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Linq;
+using System.Text;
+using Nancy;
+using Nancy.Hosting.Self;
+using System.Collections.Generic;
+using Nancy.Extensions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using Nancy.Conventions;
+
+namespace SimpleWebServer
+{
+
+
+    public class ApplicationBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("view", @"view"));
+            base.ConfigureConventions(nancyConventions);
+        }
+    }
+
+    public class LimitedQueue<T> : Queue<T>
+    {
+        public int maxNumber = 1000000;
+
+        public void Enqueue(T item)
+        {
+            if (Count == maxNumber)
+                Dequeue();
+            base.Enqueue(item);
+        }
+
+    }
+
+    public class MinerCommand   
+    {
+        enum CommandType
+        {
+
+        }
+
+    }
+
+    public class MinerInformation
+    {
+
+        public string name = "nameless miner";
+        public string internet_ip = "no internet ip";
+        public string local_ip = "no local ip";
+        public int totalHashRate = 0;
+        public List<int> hashRates;
+        public List<int> temps;
+        public long time;
+
+        public MinerInformation()
+        {
+            hashRates = new List<int>();
+            temps = new List<int>();
+        }
+
+    }
+
+
+    public class Miner
+    {
+        public string name = "noname";
+        public List<MinerCommand> waitingCommands;
+       // LimitedQueue<MinerInformation> info;
+        public LimitedQueue<JObject> info;
+
+        public Miner()
+        {
+            waitingCommands = new List<MinerCommand>();
+            info = new LimitedQueue<JObject>();
+        }
+    }
+
+    public class Server
+    {
+        private static Server instance;
+
+        public Dictionary<string, Miner> miners;
+        public JObject config;
+
+        private Server() {
+
+            miners = new Dictionary<string, Miner>();
+
+        }
+
+        public static string SHA512(string input)
+        {
+            var bytes = System.Text.Encoding.UTF8.GetBytes(input);
+            using (var hash = System.Security.Cryptography.SHA512.Create())
+            {
+                var hashedInputBytes = hash.ComputeHash(bytes);
+
+                // Convert to text
+                // StringBuilder Capacity is 128, because 512 bits / 8 bits in byte * 2 symbols for byte 
+                var hashedInputStringBuilder = new System.Text.StringBuilder(128);
+                foreach (var b in hashedInputBytes)
+                    hashedInputStringBuilder.Append(b.ToString("X2"));
+                return hashedInputStringBuilder.ToString();
+            }
+        }
+
+        public Boolean CheckPassword(string miner, string password)
+        {
+
+            if (Server.SHA512(config["password"].ToString()) == password)
+                    return true;
+            else
+                return false;
+        }
+
+        public static Server Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Server();
+                }
+                return instance;
+            }
+        }
+    }
+
+    public class SimpleModule : Nancy.NancyModule
+    {
+        
+
+        public SimpleModule()
+        {
+
+            Get["/"] = _ => View["index"];
+            Post["/minerInfoUpload"] = paramaters =>
+            {
+
+                JObject minerInfo = JObject.Parse(this.Request.Body.AsString());
+                if (!Server.Instance.CheckPassword(minerInfo["name"].ToString(), minerInfo["password"].ToString()))
+                    return "Wrong password or miner name!";
+
+                Miner miner = null;
+                Server.Instance.miners.TryGetValue(minerInfo["name"].ToString(), out miner);
+                if (miner == null)
+                {
+                    miner = new Miner();
+                    miner.name = minerInfo["name"].ToString();
+                    Server.Instance.miners.Add(minerInfo["name"].ToString(), miner);
+                }
+                miner.info.Enqueue(minerInfo);
+
+                Console.WriteLine("Miner Info: " + minerInfo.ToString());
+
+                return miner.name +" Recorded";
+
+            };
+
+            Get["/minerStats/{seconds}"] = param =>
+            {
+                List<JObject> answers = new List<JObject>();
+
+
+                DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+                TimeSpan diff = DateTime.Now.ToUniversalTime() - origin;
+                double now = Math.Floor(diff.TotalSeconds);
+                int seconds = param["seconds"];
+                foreach (var a in Server.Instance.miners)
+                {
+
+                    var res = from item in a.Value.info
+                              where item["time"].Value<double>() > (now - seconds)
+                              select item;
+
+                    answers.AddRange(res.ToList());
+                    
+                }
+
+                var array = JArray.FromObject(answers);
+
+                return array.ToString();
+            };
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            using (StreamReader r = new StreamReader("config.json"))
+            {
+                string json = r.ReadToEnd();
+                Server.Instance.config = JObject.Parse(json);
+            }
+
+            HostConfiguration hostConf = new HostConfiguration();
+            hostConf.UrlReservations = new UrlReservations() { CreateAutomatically = true };
+            hostConf.RewriteLocalhost = true;
+            string address = "http://localhost:" + Server.Instance.config["port"].ToString();
+            var apiHost = new NancyHost(hostConf, new Uri(address));
+
+            apiHost.Start();
+            Console.WriteLine("Server started on: " + address);
+            Console.ReadKey();
+
+            apiHost.Stop();
+        }
+
+
+
+
+    }
+}
